b988d620bd70284c7674e7a94e107c456c940dd0
Yussuf Shaikh <yussuf@us.ibm.com>
2020-09-04 17:38:24 +0530

    Update the chrony overwrite to bool (#58)
    Fixes #57
    
    Signed-off-by: Yussuf Shaikh <yussuf@us.ibm.com>

--- playbooks/roles/ocp-config/files/update_ignition_bootstrap.py
+++ playbooks/roles/ocp-config/files/update_ignition_bootstrap.py
@@ -21,7 +21,7 @@ if os.path.isfile('/tmp/chrony.conf.tmp'):
                 'source': 'data:text/plain;charset=utf-8;base64,' + chrony_b64,
                 'verification': {}
                 },
-            'overwrite': 'true'
+            'overwrite': True
         })
 
 ignition['storage']['files'] = files;
